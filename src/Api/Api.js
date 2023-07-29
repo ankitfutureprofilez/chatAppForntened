9a03efb72e89ab4be5981fc6195279f8dcd6bfcd
ankit jain <ankit.jain@futureprofilez.com>
2023-07-29 20:32:53 +0530

    aaa

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,7 +3,7 @@ const tokens = localStorage.getItem("token");
 const baseURL=process.env.URL
 console.log(baseURL)
 let Api = axios.create({
-    baseURL: {baseURL},
+    baseURL: baseURL,
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${tokens}`
