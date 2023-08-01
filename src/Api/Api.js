716ebfa2e86cc5893d427b349a35defb86be7cc6
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-01 16:10:30 +0530

    vercel

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const tokens = localStorage.getItem('token');
 console.log("REACT_APP_BASE_URL Api",process.env.REACT_APP_BASE_URL)
 let Api = axios.create({
-  baseURL: "https://chat-appbackend.vercel.app",
+  baseURL: "http://chat-appbackend.vercel.app",
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${tokens}`
