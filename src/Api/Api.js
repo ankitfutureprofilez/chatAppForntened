e5827569488be0b236b3880e83f357255ac95baf
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-02 16:19:22 +0530

    ddd

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 const tokens = localStorage.getItem('token');
-const baseURL = process.env.REACT_APP_BASE_URL
-console.log("REACT_APP_BASE_URL",baseURL)
+const BASE_URL = process.env.REACT_APP_BASE_URL
+console.log("REACT_APP_BASE_URL",BASE_URL)
 
 let Api = axios.create({
-  baseURL: baseURL,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${tokens}`
