adaff5f95fc373540cead13f56e95ff22f83f424
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-02 14:32:54 +0530

    vercel

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const tokens = localStorage.getItem('token');
-console.log("REACT_APP_BASE_URL Api",process.env.REACT_APP_BASE_URL)
+console.log("REACT_APP_BASE_URL",process.env.REACT_APP_BASE_URL)
 let Api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
