9c951da0a528ad5f6d726dba936ca7097765ce00
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2023-08-02 17:36:22 +0530

    ddd

--- src/message/Reciver.js
+++ src/message/Reciver.js
@@ -25,7 +25,7 @@ function Reciver(props) {
    // const socket = io("https://chat-appbackend.vercel.app");
 
     //socket = io.connect("http://localhost:8080/");
-    const socket = io("https://chat-appbackend.vercel.app/");
+    const socket = io("https://chat-appbackend.vercel.app");
 
 
     // Helper function to wrap the first letter of a username in a <div>
